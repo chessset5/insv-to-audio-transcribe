d3018a03a97c70988f6a8fca1beb549cab038d36
Aaron Shackelford <aaron.shackelford@wateraic.com>
2025-02-14 14:10:13 -0800

    Updating name to FileNameFunctions to make it easier to import into other files

--- apps/FileNameFunctions.py
+++ apps/FileNameFunctions.py
@@ -1,4 +1,40 @@
 import os
+
+
+def get_path_components(file_path: str) -> dict[str, str]:
+    '''
+    get_path_components returns the string components of a dir<br>
+    
+
+    Args:
+        file_path (str): file path to be processed
+
+    Returns:
+        dict\[str,str\]:
+            `"dir"`: directory<br>
+            `"filename"`: name of the file, no extension<br>
+            `"ext"`: the extension of the file<br>
+    '''
+    # Split the file path into directory, base name, and extension
+    directory, base_name = os.path.split(file_path)
+    name, ext = os.path.splitext(base_name)
+
+    return {"dir": directory, "filename": name, "ext": ext}
+
+
+def get_base_file_name(file_path: str) -> str:
+    """
+    getBaseName returns the file name of a path, no ext
+
+    Args:
+        file (str): file path
+
+    Returns:
+        str: file name
+    """
+    return get_path_components(file_path=file_path)["filename"]
+
+
 def increment_filename(file_path: str, template: str = " ({counter})") -> str:
     """
     increment_filename increments given file
